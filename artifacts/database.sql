dac37d0a3af2b13d2aca1550ea7839f76adfec90
unknown <paulkievits@hotmail.com>
2017-12-28 21:00:24 +0100

    DB script fix

--- artifacts/database.sql
+++ artifacts/database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `zicht` /*!40100 COLLATE 'utf8_general_ci' */
+CREATE DATABASE `zicht` /*!40100 COLLATE 'utf8_general_ci' */;
 
 CREATE USER 'zicht'@'localhost' IDENTIFIED BY 'df5cce6f05e4b021a0ed2572d173229e';
 GRANT SELECT, INSERT, DELETE, UPDATE  ON `zicht`.* TO 'zicht'@'localhost';
